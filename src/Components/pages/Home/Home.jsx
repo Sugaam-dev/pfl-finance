343f6e0644658be99dfd55d1106a6624adbcb14e
Dhurjoti Ghosh <sugaam.dev@gmail.com>
2025-10-23 10:49:14 +0530

    updated

--- src/Components/pages/Home/Home.jsx
+++ src/Components/pages/Home/Home.jsx
@@ -1,40 +1,27 @@
 import React from "react";
-
 import HeroSection from "./HeroSection";
 import RequestCallBack from "./RequestCallBack";
 import TestimonialsTeam from "./TestimonialsTeam";
 import WhyChooseUs from "./WhyChooseUs";
-
-
 import ImageHoverGallery from "./ImageHoverGallery";
 import TradesStocks from "./TradesStocks";
 import EntrepreneurHero from "./EntrepreneurHero";
 
-
-
-export default function Home() {
+       export default function Home() {
 
 
   return (
 
    <>
        <HeroSection/>
-
-  <ImageHoverGallery/>
- 
-        <TradesStocks/>
-
-    <TestimonialsTeam/>
-    <WhyChooseUs/>
-
-
-
-    {/* <main className="min-h-screen"> */}
-  
-     
-        <EntrepreneurHero/>
-    <RequestCallBack/>
-    {/* </main> */}
+       <ImageHoverGallery/>
+       <TradesStocks/>
+       <TestimonialsTeam/>
+       <WhyChooseUs/>
+{/* <main className="min-h-screen"> */}
+       <EntrepreneurHero/>
+       <RequestCallBack/>
+       {/* </main> */}
    </>
   );
 }
